f4b27b2bd68f71b4478c0a164fb610b9c2965c9c
miniengineer <samadova.minira@gmail.com>
2021-11-25 19:41:45 +0900

    authentication solution #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  namespace :api do
-    namespace :v1 do
-      devise_scope :user do
-        post 'sign_up', to: 'registrations#create'
-        post 'sign_in', to: 'sessions#create'
-      end
-    end
-  end
+  devise_for :users, path: 'api/v1/users', controllers:
+  {
+    sessions: 'api/v1/sessions',
+    registrations: 'api/v1/registrations'
+  }
 end
